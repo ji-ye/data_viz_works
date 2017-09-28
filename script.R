186a3700756e139fa0fea4a603c7b999e37994f9
jiyecapp <jiyechn@gmail.com>
2017-09-28 00:20:31 -0500

    installed packages and loaded data.
    1. Open RStudio and Install Pertinent Packages.
    2. Load the FARS Data into R.

---
+++ script.R
@@ -0,0 +1,13 @@
+# Install required packages.
+install.packages("easypackages")
+library(easypackages)
+packages("readr", "haven", "dplyr", "tidyr", "stringr", "ggplot2")
+
+getwd()
+setwd("./Github/data_viz_works/")
+
+acc2015 <- read_csv(dir()[[1]])
+acc2014 <- read_sas(dir()[[2]])
+
+class(acc2014)
+class(acc2015)
