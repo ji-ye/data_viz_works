7e97268022eeff7c551db2633f1f6eabb28720dc
jiyecapp <jiyechn@gmail.com>
2017-10-09 02:35:57 -0500

    code for assignment 2

--- DV-assignment2/three_charts.R
+++ DV-assignment2/three_charts.R
@@ -20,20 +20,39 @@ coinbase <- mutate(coinbase, volume_usd = volume_btc * price)
 # sample down to 1 percent
 coinbase_lim <- sample_frac(coinbase, size = 0.01)
 
-# Chart one - line chart
-ggplot(data=coinbase_lim) + 
-  geom_line(size=.25,aes(Time, price), color="#4b4b4b") +
-  geom_vline(xintercept = as.POSIXct("2017-03-25"), colour="#ff7575", size=.75) +
-  geom_smooth(aes(Time, price), span=20, linetype="dashed", color="#9eb8d8") +
+# aggregate trading volume data into month
+packages("xts")
+ts <- xts(coinbase_lim$volume_usd, as.Date(coinbase_lim$Date, "%Y-%m-%d"))
+ts_d = apply.daily(ts, FUN=sum)
+ts_m = apply.daily(ts_d, FUN=sum)
+df_m <- data.frame(date=index(ts_m), coredata(ts_m))
+
+# Charts - Bitcoin exchange price & volume
+# note: color #85bb65" is known as dollar bill.
+p <- ggplot(data=coinbase_lim) + 
+  geom_line(size=.25,aes(Time, price), color="#325a8c") +
+  geom_point(data=df_m, aes(as.POSIXct(date), size=100, col1/5000, alpha=.9), color="#85bb65") + 
+  geom_vline(xintercept = as.POSIXct("2017-03-25"), colour="#ff7575", size=.75, alpha=.75) +
+  geom_smooth(aes(Time, price), span=2, linetype="longdash", color="#4c88d3", alpha=.5) +
   scale_x_datetime(name = "", date_breaks = "3 month") +
-  scale_y_continuous(name ="Bitcoin to USD Exchange Rate", breaks = c(0, 500, 1000, 1500, 2000, 2500, 3000)) +
-  ggtitle("Bitcoin Price Trend", subtitle = "Bitcoin price experienced the largest surge in history this year") +
+  scale_y_continuous(name ="Bitcoin Hourly Exchange Rate in USD",
+                     breaks = c(0, 500, 1000, 1500, 2000, 2500, 3000),
+                     sec.axis = sec_axis(~.*5, name = "Monthly Trading Volume in USD (thousands)")) +
+  ggtitle("Bitcoin Price & Volume", subtitle = "both experienced the greatest surge in history this year") +
+  annotate(geom="text",x=as.POSIXct("2017-05-30"),
+           y=675,label="2017-03-25 Surge starts",colour="#ff7575", fontface="bold", alpha=.85) +
+  annotate(geom="text",x=as.POSIXct("2015-2-08"),
+           y= 3100,label="Data Source: Coinbase API", size=3.5, family = "mono", colour = "#666666") +
   theme(panel.grid.major.y = element_line( size=.1, color="#666666"),
         panel.grid.major.x = element_line(),
         panel.background = element_blank(),
         plot.title = element_text(size=18, family = "Helvetica", colour = "#3a3a3a", face = "bold"),
-        plot.subtitle = element_text(size=12, family = "mono", colour = "#666666")) +
-  annotate(geom="text",x=as.POSIXct("2017-05-03"),
-           y=800,label="Surge starts",colour="#ff7575", fontface="bold") +
-  annotate(geom="text",x=as.POSIXct("2015-2-12"),
-           y= 3100,label="Data Source: Coinbase API", size=3.5, family = "mono", colour = "#666666")
+        plot.subtitle = element_text(size=12, family = "mono", colour = "#666666"),
+        axis.title.y.right = element_text(color="#85bb65"),
+        axis.text.y.right = element_text(color="#85bb65"),
+        axis.text.y = element_text(color="#325a8c"),
+        axis.title.y = element_text(color="#325a8c"),
+        axis.ticks.y = element_line( size=.25, color="#666666"),
+        axis.text.x = element_text(color="#7f7f7f"),
+        legend.position = "none",
+        plot.margin = unit(c(1,1,1,1), "cm"))
